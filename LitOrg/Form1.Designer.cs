6e9c944b50a5a4f226a544fc35b5cf4db03dad49
Mjaksic <matej56@gmail.com>
2020-01-19 21:52:52 +0100

    Add project files.

---
+++ LitOrg/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace LitOrg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtboxNmbrOfPages = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtboxBinding = new System.Windows.Forms.TextBox();
+            this.txtboxPublisher = new System.Windows.Forms.TextBox();
+            this.txtboxYearOI = new System.Windows.Forms.TextBox();
+            this.txtboxISBN = new System.Windows.Forms.TextBox();
+            this.txtboxTitle = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxAuthor = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.rbtnDsc = new System.Windows.Forms.RadioButton();
+            this.rbtnAsc = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbFilters = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtboxNmbrOfPages);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtboxBinding);
+            this.panel1.Controls.Add(this.txtboxPublisher);
+            this.panel1.Controls.Add(this.txtboxYearOI);
+            this.panel1.Controls.Add(this.txtboxISBN);
+            this.panel1.Controls.Add(this.txtboxTitle);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtboxAuthor);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(728, 225);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtboxNmbrOfPages
+            // 
+            this.txtboxNmbrOfPages.Location = new System.Drawing.Point(152, 196);
+            this.txtboxNmbrOfPages.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxNmbrOfPages.Name = "txtboxNmbrOfPages";
+            this.txtboxNmbrOfPages.Size = new System.Drawing.Size(572, 22);
+            this.txtboxNmbrOfPages.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 199);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Number of Pages";
+            // 
+            // txtboxBinding
+            // 
+            this.txtboxBinding.Location = new System.Drawing.Point(152, 164);
+            this.txtboxBinding.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxBinding.Name = "txtboxBinding";
+            this.txtboxBinding.Size = new System.Drawing.Size(572, 22);
+            this.txtboxBinding.TabIndex = 10;
+            // 
+            // txtboxPublisher
+            // 
+            this.txtboxPublisher.Location = new System.Drawing.Point(152, 132);
+            this.txtboxPublisher.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxPublisher.Name = "txtboxPublisher";
+            this.txtboxPublisher.Size = new System.Drawing.Size(572, 22);
+            this.txtboxPublisher.TabIndex = 9;
+            // 
+            // txtboxYearOI
+            // 
+            this.txtboxYearOI.Location = new System.Drawing.Point(152, 100);
+            this.txtboxYearOI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxYearOI.Name = "txtboxYearOI";
+            this.txtboxYearOI.Size = new System.Drawing.Size(572, 22);
+            this.txtboxYearOI.TabIndex = 8;
+            // 
+            // txtboxISBN
+            // 
+            this.txtboxISBN.Location = new System.Drawing.Point(152, 68);
+            this.txtboxISBN.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxISBN.Name = "txtboxISBN";
+            this.txtboxISBN.Size = new System.Drawing.Size(572, 22);
+            this.txtboxISBN.TabIndex = 7;
+            // 
+            // txtboxTitle
+            // 
+            this.txtboxTitle.Location = new System.Drawing.Point(152, 36);
+            this.txtboxTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxTitle.Name = "txtboxTitle";
+            this.txtboxTitle.Size = new System.Drawing.Size(572, 22);
+            this.txtboxTitle.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 167);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Binding";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Publisher";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Year of Issue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 71);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ISBN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Author";
+            // 
+            // txtboxAuthor
+            // 
+            this.txtboxAuthor.Location = new System.Drawing.Point(152, 4);
+            this.txtboxAuthor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtboxAuthor.Name = "txtboxAuthor";
+            this.txtboxAuthor.Size = new System.Drawing.Size(572, 22);
+            this.txtboxAuthor.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Controls.Add(this.btnAddNew);
+            this.panel2.Controls.Add(this.btnFind);
+            this.panel2.Location = new System.Drawing.Point(752, 13);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 79);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(112, 43);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(4, 43);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(4, 7);
+            this.btnAddNew.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(100, 28);
+            this.btnAddNew.TabIndex = 1;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(112, 7);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(100, 28);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView);
+            this.panel3.Location = new System.Drawing.Point(16, 247);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(953, 292);
+            this.panel3.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(7, 4);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.Size = new System.Drawing.Size(942, 284);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnFilter);
+            this.panel4.Controls.Add(this.rbtnDsc);
+            this.panel4.Controls.Add(this.rbtnAsc);
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Controls.Add(this.cbFilters);
+            this.panel4.Location = new System.Drawing.Point(752, 105);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(217, 138);
+            this.panel4.TabIndex = 3;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(65, 100);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(100, 28);
+            this.btnFilter.TabIndex = 4;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // rbtnDsc
+            // 
+            this.rbtnDsc.AutoSize = true;
+            this.rbtnDsc.Location = new System.Drawing.Point(103, 32);
+            this.rbtnDsc.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnDsc.Name = "rbtnDsc";
+            this.rbtnDsc.Size = new System.Drawing.Size(54, 21);
+            this.rbtnDsc.TabIndex = 3;
+            this.rbtnDsc.TabStop = true;
+            this.rbtnDsc.Text = "DSC";
+            this.rbtnDsc.UseVisualStyleBackColor = true;
+            // 
+            // rbtnAsc
+            // 
+            this.rbtnAsc.AutoSize = true;
+            this.rbtnAsc.Location = new System.Drawing.Point(8, 32);
+            this.rbtnAsc.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnAsc.Name = "rbtnAsc";
+            this.rbtnAsc.Size = new System.Drawing.Size(53, 21);
+            this.rbtnAsc.TabIndex = 2;
+            this.rbtnAsc.TabStop = true;
+            this.rbtnAsc.Text = "ASC";
+            this.rbtnAsc.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 12);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Filter by";
+            // 
+            // cbFilters
+            // 
+            this.cbFilters.FormattingEnabled = true;
+            this.cbFilters.Items.AddRange(new object[] {
+            "Author",
+            "Title",
+            "ISBN",
+            "Year of Issue",
+            "Publisher"});
+            this.cbFilters.Location = new System.Drawing.Point(4, 60);
+            this.cbFilters.Margin = new System.Windows.Forms.Padding(4);
+            this.cbFilters.Name = "cbFilters";
+            this.cbFilters.Size = new System.Drawing.Size(207, 24);
+            this.cbFilters.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 554);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LitOrg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtboxNmbrOfPages;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtboxBinding;
+        private System.Windows.Forms.TextBox txtboxPublisher;
+        private System.Windows.Forms.TextBox txtboxYearOI;
+        private System.Windows.Forms.TextBox txtboxISBN;
+        private System.Windows.Forms.TextBox txtboxTitle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxAuthor;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.RadioButton rbtnDsc;
+        private System.Windows.Forms.RadioButton rbtnAsc;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbFilters;
+    }
+}
+
---
+++ LitOrg/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace LitOrg
+{
+    public partial class Form1 : Form
+    {
+        private SqlCeCommand _command;
+        private SqlCeConnection _connection;
+        private SqlCeDataAdapter _adapter;
+        private string _connectionString = @"Data Source=C:\Users\Matej\source\repos\LitOrg\dbLitOrg.sdf";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //variable koje ce se koristiti
+        String author, title, isbn, yearOi, publisher, binding, nmbrOfPgs;
+
+        //FILTER
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            String selectedFilter = cbFilters.SelectedText;
+
+            getAllDataFromTxtFields();
+
+            //query upit sa ovim podatcima
+            if (rbtnAsc.Checked && !rbtnDsc.Checked)
+            {
+                //filtrira uzlazno
+            }
+            else if (!rbtnAsc.Checked && rbtnDsc.Checked)
+            {
+                //filtrira silazno
+            }
+        
+        }
+
+        //DELETE
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            getAllDataFromTxtFields();
+            //query upit sa ovim podatcima
+        }
+
+        //UPDATE
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            getAllDataFromTxtFields();
+            //query upit sa ovim podatcima
+        }
+
+        //FIND
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            getAllDataFromTxtFields();
+            //query upit sa ovim podatcima
+        }
+
+        //ADD
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            getAllDataFromTxtFields();
+            _connection = new SqlCeConnection(_connectionString);
+            _connection.Open();
+            //_command = new SqlCeCommand("INSERT INTO books (isbn, title, author, year, publisher, binding, pagenumber) VALUES ('9780590353403', 'Harry Potter And The Sorcerer's Stone', 'J.K. Rowling', '1998', 'Scholastic Press', 'Hardcover', '309'");
+            _adapter = new SqlCeDataAdapter("SELECT * FROM books", _connection);
+            DataTable table = new DataTable();
+            _adapter.Fill(table);
+            dataGridView.DataSource = table;
+            dataGridView.AutoResizeColumns();
+            _connection.Close();
+            //query upit sa ovim podatcima
+        }
+
+
+
+        //metode
+        private void getAllDataFromTxtFields()
+        {
+            author = txtboxAuthor.Text.ToString();
+            title = txtboxTitle.Text.ToString();
+            isbn = txtboxISBN.Text.ToString();
+            yearOi = txtboxYearOI.Text.ToString();
+            publisher = txtboxPublisher.Text.ToString();
+            binding = txtboxBinding.Text.ToString();
+            nmbrOfPgs = txtboxNmbrOfPages.Text.ToString();
+        }
+    }
+}
